b39fba22c6c86afb0a3458659ee78db8a6f47895
Pav Sidhu <pav@pavsidhu.com>
2019-07-07 18:26:12 +0100

    Add global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,27 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { createGlobalStyle } from "styled-components"
 
-const Layout = ({ children }) => <main>{children}</main>
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+
+  html, body {
+    font-size: 62.5%;
+    background: #fefefe;
+  }
+`
+
+const Layout = ({ children }) => (
+  <>
+    <GlobalStyle />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
