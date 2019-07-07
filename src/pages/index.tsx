958686de60515a6d7d672b22b057f68981dcbe33
Pav Sidhu <pav@pavsidhu.com>
2019-07-07 23:49:10 +0100

    Add main index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import styled from "styled-components"
+
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+import logo from "../images/logo.svg"
+
+const Main = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fefefe;
+  border-top: 12px solid #caeaf8;
+  border-right: 12px solid #f8edca;
+  border-bottom: 12px solid #f8caca;
+  border-left: 12px solid #e4d0f2;
+`
+
+const Logo = styled.img`
+  width: 50vw;
+  max-height: 200px;
+`
+
+const Heading = styled.h1`
+  margin-top: 32px;
+  font-size: 1.8rem;
+  font-weight: normal;
+`
+
+const Index = () => (
+  <Layout>
+    <Seo title="Sign In" />
+
+    <Main>
+      <Logo src={logo} alt="Feeling logo" />
+      <Heading>👋 Coming soon to Android</Heading>
+    </Main>
+  </Layout>
+)
+
+export default Index
