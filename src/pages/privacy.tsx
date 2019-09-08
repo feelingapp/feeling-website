8e1dd747fc48586ba4337baa47deaeb165c31c66
Pav Sidhu <pav@pavsidhu.com>
2019-09-08 11:40:39 +0100

    Add dummy terms and privacy pages

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Layout } from "../components"
+
+export default function Privacy() {
+  return (
+    <Layout>
+      <h1>Privacy</h1>
+    </Layout>
+  )
+}
---
+++ src/pages/terms.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Layout } from "../components"
+
+export default function Terms() {
+  return (
+    <Layout>
+      <h1>Terms</h1>
+    </Layout>
+  )
+}
